aae16f0f87b1cf82204ff3dafd6d549b4c71a76a
Pavel Sokolkov <psokolkow@gmail.com>
2021-01-11 11:37:04 +0300

    Fixed manual build process

--- build_script.sh
+++ build_script.sh
@@ -2,6 +2,7 @@
 
 set -ex
 
+rmdir -rf build || true
 mkdir build || true
 cmake CMakeLists.txt -B build
 cd build || exit
--- main.c
+++ main.c
@@ -7,20 +7,20 @@
 
 int main() {
 
-    map_entry_t map_entry = gen_dummy_map();
-    cluster_map_t * cluster_map = build_map(map_entry);
-
-    bucket_t * bucket = cluster_map->root;
-
-    printf("%d %d %d\n", bucket->class, bucket->type,
-           bucket->c(1, 1, cluster_map->root));
-
-    for (int i = 0; i < INNER_SIZE; i++) {
-        device_t device = *bucket->inner_buckets[i]->device;
-        printf("%d %d\n", device.port, device.capacity);
-    }
-
-    int a = 1;
-    printf("%lu %lu %lu\n", sizeof(int), sizeof(void *), sizeof((void *)(a)));
+//    map_entry_t map_entry = gen_dummy_map();
+//    cluster_map_t * cluster_map = build_map(map_entry);
+//
+//    bucket_t * bucket = cluster_map->root;
+//
+//    printf("%d %d %d\n", bucket->class, bucket->type,
+//           bucket->c(1, 1, cluster_map->root));
+//
+//    for (int i = 0; i < INNER_SIZE; i++) {
+//        device_t device = *bucket->inner_buckets[i]->device;
+//        printf("%d %d\n", device.port, device.capacity);
+//    }
+//
+//    int a = 1;
+//    printf("%lu %lu %lu\n", sizeof(int), sizeof(void *), sizeof((void *)(a)));
     return 0;
 }
